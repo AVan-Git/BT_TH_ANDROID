4c5f843c3866dd38e9ff30cc68a9532d8ed2d986
nguyenvangithub <81097647+nguyenvangithub@users.noreply.github.com>
2022-02-18 10:05:24 +0700

    BT thuc hanh tuan 1 (a-e)

---
+++ app/src/main/java/com/example/th_didong1/Activity_TH1_E.java
@@ -0,0 +1,14 @@
+package com.example.th_didong1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_TH1_E extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_th1_e);
+    }
+}
